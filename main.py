ed882c12248d7c30512671682ccaeb85d9c238e1
evelynloza9 <evelyn.morales.009@gmail.com>
2016-07-25 14:57:23 -0700

    added to main handler

--- main.py
+++ main.py
@@ -10,9 +10,35 @@ jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(template_d
 
 >>>>>>> fe5badf1fef66fc22f70a7b2d292d01940ef86a2
 
+#model for message
+class Reminder(ndb.Model):
+    reminder = ndb.TextProperty()
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        #show a list of the reminders
+        reminder = Reminder.query().fetch()
+
+        #render response
+        template_values= {'reminder':reminder}
+
+        template = jinja_environment.get_template('home.html')
+        self.response.write(template.render(template_values))
+
+    #post method that adds reminder into database
+    def post(self):
+        #get reminder from form
+        reminder = self.request.get('reminder')
+
+        #saves & puts the reminder from form into the database
+        new_reminder = Reminder(reminder = reminder)
+        new_reminder.put()
+
+        #shows the main page after you press submit
+        self.redirect("/")
+
+#add time stamp and use it to order messages
+#log in and log out links
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
