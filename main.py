be989f1b0ad92a2c2e0b56a0e149f2eaab99672f
Gretal Dea <deagretal@gmail.com>
2016-07-26 15:08:59 -0700

    added date to reminders

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ from google.appengine.api import users
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir))
 
-
-#model for message
 class User(ndb.Model):
     email = ndb.StringProperty()
     #a method that return the key url for a user when called
@@ -23,6 +21,9 @@ class Reminder(ndb.Model):
     title = ndb.TextProperty()
     description = ndb.TextProperty()
     frequency = ndb.IntegerProperty()
+    month = ndb.IntegerProperty()
+    day = ndb.IntegerProperty()
+    year = ndb.IntegerProperty()
     user_key = ndb.KeyProperty(kind=User)
 
 
@@ -58,11 +59,14 @@ class ReminderHandler(webapp2.RequestHandler):
         title = self.request.get('title')
         description = self.request.get('description')
         frequency = self.request.get('frequency')
+        month = self.request.get('month')
+        day = self.request.get('day')
+        year = self.request.get('year')
         urlsafe_key = self.request.get('key')
         key = ndb.Key(urlsafe=urlsafe_key)
 
         #put the reminders from form into the database
-        new_reminder = Reminder(title = title,description = description,frequency = int(frequency),user_key=key)
+        new_reminder = Reminder(title=title, description=description, frequency=int(frequency), month=month, day=day, year=year, user_key=key)
         new_reminder.put()
 
         #shows the home page after you press submit
