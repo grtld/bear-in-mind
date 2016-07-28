d79656a442d4193383e09385630ccf148ce837b6
evelynloza9 <evelyn.morales.009@gmail.com>
2016-07-28 09:33:46 -0700

    fixed prob

--- main.py
+++ main.py
@@ -189,15 +189,6 @@ class LoginHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template('login.html')
         self.response.write(template.render())
-class SettingsHandler(webapp2.RequestHandler):
-    def get(self):
-        if (auth2.isSignedIn.get()) {
-          var profile = auth2.currentUser.get().getBasicProfile();
-          full_name = profile.getName());
-          image_url = profile.getImageUrl());
-          console.log('Email: ' + profile.getEmail());
-        }
-    def post(self):
 
 app = webapp2.WSGIApplication([
     ('/addreminder', ReminderHandler),
@@ -206,5 +197,4 @@ app = webapp2.WSGIApplication([
     ('/', LoginHandler),
     ('/addphone', AddPhoneHandler),
     ('/sendtext', SendTextHandler),
-    ('/settings',SettingsHandler),
 ], debug=True)
